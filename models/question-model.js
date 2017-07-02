785e8cdb1e6e3836410c9272bd603e376eccea42
Paolo Herrera <paolo.herrera.araya@gmail.com>
2017-07-01 21:05:25 -0400

    Survey ahora si que si

--- models/question-model.js
+++ models/question-model.js
@@ -1,6 +1,6 @@
 'use strict'
 
-var conn = require('./yami-model')
+var conn = require('./yami-model'),
     QuestionModel = () => {}
 
 QuestionModel.getQuestions = (data, cb) => conn.query('SELECT * FROM Question WHERE ?', data, cb)
