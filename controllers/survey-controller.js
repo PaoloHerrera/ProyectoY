a230884a2c7d54f022453ab2452b98328398cadb
Paolo Herrera <paolo.herrera.araya@gmail.com>
2017-07-01 21:17:11 -0400

    prueba json dinamico 1

--- controllers/survey-controller.js
+++ controllers/survey-controller.js
@@ -72,7 +72,7 @@ SurveyController.addForm = (req, res, next) => {
                             jsonQuestion.push(item)
                           }
 
-                          console.log(jsonQuestion)
+                          console.log([jsonQuestion])
                           /*res.render('survey', {
                             idSurvey : questionsRow[0].idSurvey,
                             question1 : questionsRow[0].question,
