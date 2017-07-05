86cc5443c948fbb1dc4e90667789c7c86c9b300a
Paolo Herrera <paolo.herrera.araya@gmail.com>
2017-07-05 14:32:56 -0400

    prueba get con angular

--- public/js/survey.js
+++ public/js/survey.js
@@ -18,7 +18,7 @@
         .then(function(response) {
             // success
             console.log(response.data)
-            $location.url('/encuesta-ok')
+            $location.url('http://yamiapp.co/encuesta-ok/')
 
         },
         function(response) { // optional
