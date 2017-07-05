dc5da2a5df45826db3a9cd9395b765414fd335d8
Paolo Herrera <paolo.herrera.araya@gmail.com>
2017-07-05 15:19:47 -0400

    prueba get con angular

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,7 @@ angular.module('app', ['app.filters', 'app.services', 'app.directives']).
   config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.
       when('/encuesta-ok', {
-        templateUrl: '../../views/errorcitos',
+        templateUrl: '../../../views/errorcitos',
         controller: SubmitSurveyController
       }).
       otherwise({
