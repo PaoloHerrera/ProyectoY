031c0c4a6f4cf72de29e37e9fae1bce2a520ab42
Paolo Herrera <paolo.herrera.araya@gmail.com>
2017-07-16 19:26:55 -0400

    test reloj

--- public/js/clock.js
+++ public/js/clock.js
@@ -1,12 +1,18 @@
-function TimeCtrl($scope, $timeout) {
-    $scope.clock = "loading clock..."; // initialise the time variable
-    $scope.tickInterval = 1000 //ms
+(function() {
+  'use strict';
 
-    var tick = function() {
-        $scope.clock = Date.now() // get the current time
-        $timeout(tick, $scope.tickInterval); // reset the timer
-    }
+  angular
+    .module('app', [])
+    .controller('TimeCtrl', ['$scope','$timeout', function($scope,$timeout) {
+      $scope.clock = "loading clock..."; // initialise the time variable
+      $scope.tickInterval = 1000 //ms
+
+      var tick = function() {
+          $scope.clock = Date.now() // get the current time
+          $timeout(tick, $scope.tickInterval); // reset the timer
+        }
 
     // Start the timer
     $timeout(tick, $scope.tickInterval);
-}
+})
+})();
