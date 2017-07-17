304385616338a8f2f1a3625fa9d2572e6ae4c803
Paolo Herrera <paolo.herrera.araya@gmail.com>
2017-07-16 21:53:15 -0400

    test countdown

--- public/js/countdown.js
+++ public/js/countdown.js
@@ -1,27 +1,24 @@
-console.log(hello);
-
 var app = angular.module('countdown', []);
 
-app.controller('CountdownCtrl', function($scope, hello) {
+app.controller('CountdownCtrl', function($scope) {
+  $scope.init = function(date) {
+    $scope.timePromo = date;
 
-  console.log(hello);
+    $scope.timeTill = {};
+    var updateClock = function(){
+        $scope.timenow = new Date();
+        $scope.seconds = ($scope.timePromo - $scope.timenow) / 1000;
 
-  $scope.timePromo = new Date(2017, 12, 31, 23, 60);
-
-  $scope.timeTill = {};
-  var updateClock = function(){
-      $scope.timenow = new Date();
-      $scope.seconds = ($scope.timePromo - $scope.timenow) / 1000;
-
-      $scope.timeTill = {
-        daysLeft: parseInt($scope.seconds / 86400),
-        hoursLeft: parseInt($scope.seconds % 86400 / 3600),
-        minutesLeft: parseInt($scope.seconds % 86400 % 3600 / 60),
-        secondsLeft: parseInt($scope.seconds % 86400 % 3600 % 60)
-      }
-    };
-    setInterval(function () {
-      $scope.$apply(updateClock);
-      }, 1000);
-      updateClock();
-    });
+        $scope.timeTill = {
+          daysLeft: parseInt($scope.seconds / 86400),
+          hoursLeft: parseInt($scope.seconds % 86400 / 3600),
+          minutesLeft: parseInt($scope.seconds % 86400 % 3600 / 60),
+          secondsLeft: parseInt($scope.seconds % 86400 % 3600 % 60)
+        }
+      };
+      setInterval(function () {
+        $scope.$apply(updateClock);
+        }, 1000);
+        updateClock();
+  }
+});
