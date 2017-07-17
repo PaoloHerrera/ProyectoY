0aedbee7dfd95169f9a3e70dfb45f68e15a3c939
Paolo Herrera <paolo.herrera.araya@gmail.com>
2017-07-16 20:40:31 -0400

    test countdown

--- public/js/countdown.js
+++ public/js/countdown.js
@@ -1,6 +1,6 @@
 var app = angular.module('countdown', []);
 
-app.controller('CountdownCtrl', function($scope,$timeout,dateFilter) {
+app.controller('CountdownCtrl', function($scope) {
 
   $scope.timePromo = {
     date: date
